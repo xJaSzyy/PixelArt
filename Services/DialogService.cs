663abfc235429360925fc92abe432572304d97e3
xjasz <novoselov.s@v2grp.ru>
2026-08-20 12:08:21 +0700

    add dialog animations

--- Services/DialogService.cs
+++ Services/DialogService.cs
@@ -13,46 +13,99 @@ public class DialogService
     private readonly DrawService _drawService;
     private readonly MouseService _mouseService;
 
-    private readonly Texture2D _pixelTexture;
+    public bool IsDialogOpen { get; private set; }
 
-    public bool IsDialogOpen { get; private set; } = false;
     private string _text = string.Empty;
     private Func<bool> _onConfirm;
-    
+
     private readonly Point _dialogSize = new(320, 196);
+
     private const int _buttonSize = 64;
     private const int _spacing = 32;
 
     private readonly Button _confirmButton;
     private readonly Button _cancelButton;
 
-    public DialogService(GraphicsDevice graphicsDevice, DrawService drawService, MouseService mouseService, ContentManager content)
+    private float _animationProgress;
+    private bool _isClosing;
+
+    private const float _openAnimationDuration = 0.25f;
+    private const float _closeAnimationDuration = 0.1f;
+
+    public DialogService(
+        GraphicsDevice graphicsDevice,
+        DrawService drawService,
+        MouseService mouseService,
+        ContentManager content)
     {
         _graphicsDevice = graphicsDevice;
         _drawService = drawService;
         _mouseService = mouseService;
 
-        _pixelTexture = new Texture2D(_graphicsDevice, 1, 1);
-        _pixelTexture.SetData([Color.White]);
-        
-        _confirmButton = new Button(content.Load<Texture2D>("Icons/confirm"),
-            new Rectangle(0, 0, _buttonSize, _buttonSize));
-        
-        _cancelButton = new Button(content.Load<Texture2D>("Icons/cancel"),
-            new Rectangle(0, 0, _buttonSize, _buttonSize));
+        var pixelTexture = new Texture2D(
+            _graphicsDevice,
+            1,
+            1);
+
+        pixelTexture.SetData([Color.White]);
+
+        _confirmButton = new Button(
+            content.Load<Texture2D>("Icons/confirm"),
+            new Rectangle(
+                0,
+                0,
+                _buttonSize,
+                _buttonSize));
+
+        _cancelButton = new Button(
+            content.Load<Texture2D>("Icons/cancel"),
+            new Rectangle(
+                0,
+                0,
+                _buttonSize,
+                _buttonSize));
     }
 
-    public void Update(MouseState mouse)
+    public void Update(MouseState mouse, GameTime gameTime)
     {
         if (!IsDialogOpen)
         {
             return;
         }
-        
+
+        var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        if (_isClosing)
+        {
+            _animationProgress -= deltaTime / _closeAnimationDuration;
+
+            if (_animationProgress <= 0f)
+            {
+                _animationProgress = 0f;
+
+                _isClosing = false;
+                IsDialogOpen = false;
+
+                _onConfirm = null;
+                _mouseService.SetMouse(mouse);
+
+                return;
+            }
+        }
+        else
+        {
+            _animationProgress += deltaTime / _openAnimationDuration;
+
+            if (_animationProgress >= 1f)
+            {
+                _animationProgress = 1f;
+            }
+        }
+
         _confirmButton.Update(mouse);
         _cancelButton.Update(mouse);
         
-        if (_mouseService.IsLeftMouseButtonClicked(mouse))
+        if (!_isClosing && _mouseService.IsLeftMouseButtonClicked(mouse))
         {
             if (_confirmButton.IsHovered)
             {
@@ -63,10 +116,10 @@ public class DialogService
                 HideDialog();
             }
         }
-        
+
         _mouseService.SetMouse(mouse);
     }
-    
+
     public void Draw(SpriteBatch spriteBatch)
     {
         if (!IsDialogOpen)
@@ -74,12 +127,31 @@ public class DialogService
             return;
         }
 
-        var halfScreenWidth = _graphicsDevice.Viewport.Width / 2;
-        var halfScreenHeight = _graphicsDevice.Viewport.Height / 2;
-        var halfButtonSize = _buttonSize / 2;
+        var screenCenter = new Vector2(_graphicsDevice.Viewport.Width / 2f, _graphicsDevice.Viewport.Height / 2f);
 
-        var dialogPosition = new Point(halfScreenWidth - _dialogSize.X / 2, halfScreenHeight - _dialogSize.Y / 2);
-        
+        var scale = _isClosing
+            ? EaseInCubic(_animationProgress)
+            : EaseOutBack(_animationProgress);
+
+        var dialogPosition = new Point(
+            (int)(screenCenter.X - _dialogSize.X / 2f),
+            (int)(screenCenter.Y - _dialogSize.Y / 2f));
+
+        var transformMatrix =
+            Matrix.CreateTranslation(
+                -screenCenter.X,
+                -screenCenter.Y,
+                0f)
+            *
+            Matrix.CreateScale(scale)
+            *
+            Matrix.CreateTranslation(
+                screenCenter.X,
+                screenCenter.Y,
+                0f);
+
+        spriteBatch.Begin(samplerState: SamplerState.PointClamp, transformMatrix: transformMatrix);
+
         _drawService.DrawRoundedRectangle(
             spriteBatch,
             new Rectangle(
@@ -90,33 +162,75 @@ public class DialogService
             new Color(45, 45, 45),
             6);
 
-        _drawService.DrawString(spriteBatch, _text, new Vector2(halfScreenWidth, dialogPosition.Y + _drawService.MeasureString(_text).Y + _spacing), Color.Yellow, 2f);
+        var textSize = _drawService.MeasureString(_text);
 
-        _confirmButton.Bounds = new Rectangle(halfScreenWidth - halfButtonSize - halfButtonSize, halfScreenHeight, _buttonSize, _buttonSize);
-        _cancelButton.Bounds = new Rectangle(halfScreenWidth, halfScreenHeight, _buttonSize, _buttonSize);
-        
+        _drawService.DrawString(
+            spriteBatch,
+            _text,
+            new Vector2(
+                screenCenter.X,
+                dialogPosition.Y + textSize.Y + _spacing),
+            Color.Yellow,
+            2f);
+
+        _confirmButton.Bounds = new Rectangle(
+            (int)screenCenter.X - _buttonSize,
+            (int)screenCenter.Y,
+            _buttonSize,
+            _buttonSize);
+
+        _cancelButton.Bounds = new Rectangle(
+            (int)screenCenter.X,
+            (int)screenCenter.Y,
+            _buttonSize,
+            _buttonSize);
+
         _confirmButton.Draw(spriteBatch, Color.Green);
+
         _cancelButton.Draw(spriteBatch, Color.IndianRed);
+
+        spriteBatch.End();
     }
 
     public void ShowDialog(string text, Func<bool> onConfirm)
     {
         IsDialogOpen = true;
+
         _text = text;
         _onConfirm = onConfirm;
+
+        _isClosing = false;
+        _animationProgress = 0f;
     }
-    
+
     private void OnConfirmButtonClicked()
     {
         if (_onConfirm?.Invoke() == true)
         {
             HideDialog();
-            _onConfirm = null;
         }
     }
 
     private void HideDialog()
     {
-        IsDialogOpen = false;
+        if (_isClosing)
+        {
+            return;
+        }
+
+        _isClosing = true;
+    }
+
+    private static float EaseOutBack(float t)
+    {
+        const float c1 = 1.70158f;
+        const float c3 = c1 + 1f;
+
+        return 1f + c3 * MathF.Pow(t - 1f, 3f) + c1 * MathF.Pow(t - 1f, 2f);
+    }
+
+    private static float EaseInCubic(float t)
+    {
+        return t * t * t;
     }
 }
